ef56fb4864dbb0c9c51faffc36279fe5aaafc309
pizzaYami <cyj9407@naver.com>
2023-04-13 10:31:56 +0900

    main에서 바꿈

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>main에서 바꾼것</code> and save to reload.
         </p>
         <a
           className="App-link"
